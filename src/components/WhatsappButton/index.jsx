da83738e3108a27dc065305a995f52c2d0ded92f
JoaoArthur25 <jarthurlsilva25@gmail.com>
2025-03-02 17:48:36 -0300

    Alterando nmr wpp

--- src/components/WhatsappButton/index.jsx
+++ src/components/WhatsappButton/index.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./WhatsappButton.css"; 
 
 const WhatsappButton = () => {
-  const whatsappLink = "https://wa.me/5591984891906"; 
+  const whatsappLink = "https://wa.me/5547989293394"; 
 
   return (
     <a href={whatsappLink} className="whatsapp-button" target="_blank" rel="noopener noreferrer">
